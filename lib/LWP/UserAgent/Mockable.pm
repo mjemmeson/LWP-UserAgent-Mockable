d560fb7c0a6930364b0f76a82e025a5c5ff99848
Michael Jemmeson <mjemmeson@cpan.org>
2015-09-22 17:44:44 +0100

    1.13

--- lib/LWP/UserAgent/Mockable.pm
+++ lib/LWP/UserAgent/Mockable.pm
@@ -8,7 +8,7 @@ use LWP::UserAgent;
 use Safe::Isa '$_isa';
 use Storable qw( dclone nstore retrieve );
 
-our $VERSION = '1.12';
+our $VERSION = '1.13';
 
 my $instance = __PACKAGE__->__instance;
 sub __instance {
